2b10293c7c1639350cd9881e8b2dee28596b4a1e
P-Burk <68164093+P-Burk@users.noreply.github.com>
2022-04-01 23:04:04 -0400

    added class info header comment

--- src/Appointment/AppointmentService.java
+++ src/Appointment/AppointmentService.java
@@ -1,3 +1,11 @@
+/*
+ * Class: CS-320-T4514 Software Test Automation & QA
+ * Instructor: Dr. Angel Cross, DIT, MMIS
+ * Assignment: 5-1 Milestone: Appointment Service
+ * Student: Preston Burkhardt
+ * Date: 01 Apr 2022
+ */
+
 package Appointment;
 
 public class AppointmentService {
