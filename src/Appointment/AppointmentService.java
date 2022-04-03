6398d05376742ee826e84c74021d5452d9681d04
P-Burk <68164093+P-Burk@users.noreply.github.com>
2022-04-02 22:32:16 -0400

    Completed AppointmentService class.

--- src/Appointment/AppointmentService.java
+++ src/Appointment/AppointmentService.java
@@ -8,5 +8,39 @@
 
 package Appointment;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class AppointmentService {
+    private final ArrayList<Appointment> apptServiceList = new ArrayList<>();
+
+    //CONSTRUCTOR
+    public AppointmentService() {}
+
+    /**
+     * Creates a new appointment and adds it to apptServiceList arraylist.
+     *
+     * @param apptDate appointment's name.
+     * @param apptDesc appointment's description.
+     */
+    public void addNewAppt(String apptDate, String apptDesc) throws ParseException {
+        Appointment newAppt = new Appointment(apptDate, apptDesc);
+        apptServiceList.add(newAppt);
+    }
+
+    /**
+     * Deletes a specific appointment from the apptServiceList arraylist.
+     *
+     * @param apptID task's ID number that you wish to delete.
+     */
+    public void deleteAppt(String apptID) {
+        for (Appointment appt : apptServiceList) {
+            if (Objects.equals(apptID, appt.getAppointmentID())) {
+                apptServiceList.remove(appt);
+                return;
+            }
+        }
+        System.out.println("Appointment not found.");
+    }
 }
